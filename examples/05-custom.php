dd774241604c543f5d49b214da77725bf0fab331
bybzmt <other@bybzmt.me>
2017-10-03 11:52:35 +0800

    example5

--- examples/05-custom.php
+++ examples/05-custom.php
@@ -17,6 +17,8 @@ class MyRouter extends \Bybzmt\Router\Router
     protected $_regex_right = '$#i';
 
     /**
+     * 路由分发执行
+     *
      * @param func 之注册路由时设置的，它可以是任意值，并且会原样传到这个参数上
      * @param params 是路由捕获到的参数，没有时为空数组
      */
@@ -29,6 +31,34 @@ class MyRouter extends \Bybzmt\Router\Router
 
         echo "这里就是示例一下，可以任意修改～\n";
         var_dump($func, $params);
+    }
+
+    /*
+     * 这是一种典型的修改
+     *
+     * 由于func的类需要有完整命名空间，所以看上去很长很累赘
+     * 但是由于路由库不能确定func在特定项目什么样更好，这里
+     * 提供一种自定义的func映射方的方式
+     *
+     * 在这里示例: 自动给class增加前缀和method增加后缀
+     *
+     * (函数应该叫: dispatch 这里只是为了方便作例子改了个名)
+     */
+    protected function dispatch2($func, array $params)
+    {
+        //快速判断func格式类型
+        //不过一般特定项目只会使用一种形式，这个判可以忽略
+        //if (is_string($func) && $func[0] == $this->_func_separator)
+
+        $tmp = explode($this->_func_separator, $func, 4);
+
+        //func格式规定最短为:class:method所以tmp[2]一定存在
+        $tmp[1] = 'Bybymt\\Blog\\Web\\' . $tmp[1];
+        $tmp[2] = $tmp[2].'Action';
+
+        $newFunc = implode($this->_func_separator, $tmp);
+
+        return parent::dispatch($newFunc, $params);
     }
 
     /*
